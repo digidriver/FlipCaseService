094907c843a7a092a456124cb65e647f61ad0dcf
digidriver <slippers99@hotmail.com>
2016-04-15 11:42:37 -0400

    2016-04-15 First commit

---
+++ ConsoleClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using ConsoleClient.ServiceReferenceWcf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FlipCaseServiceClient client = new FlipCaseServiceClient("BasicHttpBinding_IFlipCaseService");
+            StringData sd = new StringData();
+            Console.WriteLine("Enter some text below. Our service will flip the upper and lower case for you!");
+            sd.OriginalString = Console.ReadLine();
+            sd = client.FlipTheCase(sd);
+            Console.WriteLine("The flipped case is " + sd.FlippedCaseString);
+            Console.ReadLine();
+        }
+    }
+}
